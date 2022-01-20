5bd279f9b622b357e2436af2c028bd86c2ddf9d8
Michael PETRE <petre.mic@gmail.com>
2022-01-20 22:22:11 +0100

    Websters English Dictionary

--- jsonit.sh
+++ jsonit.sh
@@ -4,4 +4,4 @@ filename=`basename $1 .csv`
 
 #{"term": "", "altterm": "", "pronunciation": "", "definition": "", "pos": "", "examples": "", "audio": ""}, {"term": "", "altterm": "", "pronunciation": "", "definition": "to separate", "pos": "", "examples": "", "audio": ""}
 
-awk -F\| ' BEGIN { print "[" } { print "{\"term\" : ",$1,", \"altterm\": \"\", \"pronunciation\": \"\", \"definition\" : ",$2,", \"pos\": \"\", \"examples\": \"\", \"audio\": \"\"}," } END { print "]" } ' ${filename}.csv > ${filename}.json
+awk -F\| ' BEGIN { print "[" } { print "{\"term\" : \"",$1,"\", \"altterm\": \"\", \"pronunciation\": \"\", \"definition\" : \"",$2,"\", \"pos\": \"\", \"examples\": \"\", \"audio\": \"\"}," } END { print "]" } ' ${filename}.csv > ${filename}.json
